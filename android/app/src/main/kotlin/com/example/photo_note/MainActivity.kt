4df3b5103e6d400d60c0b33ae0d6e7d76d216d14
Efim Sivolap <efimsivolap@MacBook-Pro-Efim-2.local>
2025-03-21 10:28:55 +0500

    add: TodosScreen

---
+++ android/app/src/main/kotlin/com/example/photo_note/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.photo_note
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/ui/screens/todos_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Список тодо'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
